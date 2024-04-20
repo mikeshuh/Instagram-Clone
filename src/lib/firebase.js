3d8a28f76902e72e7a466fbe2ff74c742434b1aa
Michael Huh <michael.stephen.huh@gmail.com>
2024-04-19 21:20:57 -0700

    seed edit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyDNO86N85dyM908RoE4gXdUPZRCL99nYNs',
-  authDomain: 'instagram-clone-ecf46.firebaseapp.com',
-  projectId: 'instagram-clone-ecf46',
-  storageBucket: 'instagram-clone-ecf46.appspot.com',
-  messagingSenderId: '360716897049',
-  appId: '1:360716897049:web:05874fca1d8dd00cbad388',
-};
+const config = {};
 
 const firebase = initializeApp(config);
 const { FieldValue } = getFirestore(firebase);
--- src/seed.js
+++ src/seed.js
@@ -1,7 +1,5 @@
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
-/* eslint-disable no-plusplus */
-// NOTE: replace 'NvPY9M9MzFTARQ6M816YAzDJxZ72' with your Firebase auth user id (can be taken from Firebase)
 export function seedDatabase(firebase) {
   const users = [
     {
@@ -42,12 +40,10 @@ export function seedDatabase(firebase) {
     },
   ];
 
-  // eslint-disable-next-line prefer-const
   for (let k = 0; k < users.length; k++) {
     addDoc(collection(getFirestore(firebase), 'users'), users[k]);
   }
 
-  // eslint-disable-next-line prefer-const
   for (let i = 1; i <= 5; ++i) {
     addDoc(collection(getFirestore(firebase), 'photos'), {
       photoId: i,
