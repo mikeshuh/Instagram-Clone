1debe424fd8bd5e8ab6772123a430cf8c1e0e1f4
Michael Huh <michael.stephen.huh@gmail.com>
2024-05-10 02:49:53 -0700

    suggested profiles

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,7 +3,12 @@ import {
   collection,
   query,
   where,
+  limit,
   getDocs,
+  doc,
+  updateDoc,
+  arrayRemove,
+  arrayUnion,
 } from 'firebase/firestore';
 import { firebase } from '../lib/firebase';
 
@@ -28,4 +33,54 @@ export async function getUserByUserId(userId) {
   }));
 
   return user;
+}
+
+export async function getSuggestedProfiles(userId, following) {
+  const db = getFirestore(firebase);
+  const usersRef = collection(db, 'users');
+  let q;
+
+  if (following.length > 0) {
+    q = query(usersRef, where('userId', 'not-in', [...following, userId]));
+  } else {
+    q = query(usersRef, where('userId', '!=', userId));
+  }
+
+  q = query(q, limit(10)); // Apply limit to the constructed query
+  const result = await getDocs(q);
+
+  const profiles = result.docs.map((user) => ({
+    ...user.data(),
+    docId: user.id,
+  }));
+
+  return profiles;
+}
+
+export async function updateLoggedInUserFollowing(
+  loggedInUserDocId,
+  profileId,
+  isFollowingProfile
+) {
+  const db = getFirestore(firebase);
+  const userDocRef = doc(db, 'users', loggedInUserDocId);
+  return updateDoc(userDocRef, {
+    following: isFollowingProfile
+      ? arrayRemove(profileId)
+      : arrayUnion(profileId),
+  });
+}
+
+export async function updateFollowedUserFollowers(
+  profileDocId,
+  loggedInUserDocId,
+  isFollowingProfile
+) {
+  const db = getFirestore(firebase);
+  const profileDocRef = doc(db, 'users', profileDocId);
+  return updateDoc(profileDocRef, {
+    followers: isFollowingProfile
+      ? arrayRemove(loggedInUserDocId)
+      : arrayUnion(loggedInUserDocId),
+  });
 }
